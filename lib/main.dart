73140feb88d1e0ed11fcc7b0d6d494c712e70b54
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2024-06-14 01:23:56 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'dart:developer';
+// import 'dart:developer';
 
 import 'package:evo_chat/firebase_options.dart';
 import 'package:evo_chat/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_notification_channel/flutter_notification_channel.dart';
-import 'package:flutter_notification_channel/notification_importance.dart';
+// import 'package:flutter_notification_channel/flutter_notification_channel.dart';
+// import 'package:flutter_notification_channel/notification_importance.dart';
 
 //global object for accessing device screen size
 late Size mq;
